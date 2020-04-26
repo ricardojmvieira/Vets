e68cabfa22480f17a458726d4b7767617991fecf
Ricardo Vieira <ricvi.ricardovieira@gmail.com>
2020-04-26 01:40:26 +0100

    Adicionado DonosController, Donos views e restrições de dados.

--- Vets/Models/Donos.cs
+++ Vets/Models/Donos.cs
@@ -14,7 +14,14 @@ namespace Vets.Models
         }
         [Key]
         public int ID { get; set; }
+
+        [Required(ErrorMessage = "O Nome é de preenchimento obrigatório.")]
+        [StringLength(40, ErrorMessage = "=O {0} não pode ter mais de {1} caracteres.")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório.")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "O {0} deve ter exatamente {1} caracteres.")]
+        [RegularExpression("[1356][0-9]{8}", ErrorMessage = "Deve escrever exatamente 9 algarismos, começados por 1, 3, 5 e 6")]
         public string NIF { get; set; }
 
         //select * from Animais a, Donos d where a.DonoFK = d.ID and d.id = ?
